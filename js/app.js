37a620d5f233b5bb19741ad82e91f0ce4676e493
Gilmar Sanchez Cuartas <gilmarsancu@gmail.com>
2020-11-01 13:04:49 -0500

    Validaciones y guardar en el objeto las citas creadas

---
+++ js/app.js
@@ -0,0 +1,116 @@
+//Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+class Citas{
+    constructor(){
+        this.citas = [];
+    }
+
+    agregarCita(cita){
+        this.citas = [...this.citas, cita];
+        console.log(this.citas);
+    }
+}
+
+class UI{
+
+    imprimirAlerta(mensaje, tipo){
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        //Agregar clase con base al tipo de error
+        if(tipo === 'error'){
+            divMensaje.classList.add('alert-danger');
+        } else{
+            divMensaje.classList.add('alert-success');
+        }
+
+        //Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        //Agregar al HTML
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        //Quitar la alerta
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 5000);
+    }
+
+}
+
+//Instancias
+const ui = new UI();
+const administrarCitas = new Citas();
+
+//Registrar eventos
+eventListeners();
+function eventListeners(){
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
+}
+
+//Objeto con la información de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+//Agregar datos al objeto cita
+function datosCita(e){
+    citaObj[e.target.name] = e.target.value;
+    console.log(citaObj);
+}
+
+//Valida y agrega una nueva cita a la clase Citas
+function nuevaCita(e){
+    e.preventDefault();
+
+    //Extraer información del objeto cita
+    const {mascota, propietario, telefono, fecha, hora, sintomas} = citaObj;
+
+    if(mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === ''){
+        ui.imprimirAlerta('Todos los campos son obligatorios', 'error');
+        return;
+    }
+
+    //Generar id único
+    citaObj.id = Date.now();
+
+    //Creando una cita
+    administrarCitas.agregarCita({...citaObj});
+
+    //Reiniciar el objeto para la validación
+    reiniciarObjeto();
+
+    //Reiniciar el formulario
+    formulario.reset();
+
+}
+
+function reiniciarObjeto(){
+    citaObj.mascota = '';
+    citaObj.propietario = '';
+    citaObj.telefono = '';
+    citaObj.fecha = '';
+    citaObj.hora = '';
+    citaObj.sintomas = '';
+}
